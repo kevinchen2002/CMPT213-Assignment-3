62ffbd2690bb6473d752162e5939f63145a62749
kevinchen2002 <kaifengchen2002@gmail.com>
2021-11-09 14:02:36 -0800

    Implements two listeners.

--- src/ca/cmpt213/a3/view/AddConsumableUI.java
+++ src/ca/cmpt213/a3/view/AddConsumableUI.java
@@ -2,6 +2,9 @@ package ca.cmpt213.a3.view;
 
 import ca.cmpt213.a3.control.ConsumableFactory;
 import ca.cmpt213.a3.model.Consumable;
+import com.github.lgooddatepicker.components.DatePicker;
+import com.github.lgooddatepicker.optionalusertools.DateChangeListener;
+import com.github.lgooddatepicker.zinternaltools.DateChangeEvent;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,7 +12,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalDateTime;
 
-public class AddConsumableUI extends JDialog implements ActionListener {
+public class AddConsumableUI extends JDialog implements ActionListener, DateChangeListener {
+    private boolean isFood = true;
+    private LocalDateTime expDate;
+
     private JComboBox<String> consumableTypeSelect;
     private JLabel weightOrVolumeLabel;
     private final String[] typeOptions = {"Food", "Drink"};
@@ -24,7 +30,7 @@ public class AddConsumableUI extends JDialog implements ActionListener {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
         consumableTypeSelect = new JComboBox<String>(typeOptions);
-        //consumableTypeSelect.setSelectedIndex(-1);
+        consumableTypeSelect.setSelectedIndex(-1);
         consumableTypeSelect.setPreferredSize(new Dimension(500, 30));
         consumableTypeSelect.addActionListener(this);
 
@@ -35,7 +41,7 @@ public class AddConsumableUI extends JDialog implements ActionListener {
         JTextField nameField = new JTextField();
         namePanel.add(nameLabel);
         namePanel.add(nameField);
-        namePanel.setPreferredSize(new Dimension(500,100));
+        namePanel.setPreferredSize(new Dimension(500,30));
 
         JPanel notesPanel = new JPanel();
         notesPanel.setLayout(new BoxLayout(notesPanel, BoxLayout.X_AXIS));
@@ -44,7 +50,7 @@ public class AddConsumableUI extends JDialog implements ActionListener {
         JTextField notesField = new JTextField();
         notesPanel.add(notesLabel);
         notesPanel.add(notesField);
-        notesPanel.setPreferredSize(new Dimension(500,100));
+        notesPanel.setPreferredSize(new Dimension(500,30));
 
         JPanel pricePanel = new JPanel();
         pricePanel.setLayout(new BoxLayout(pricePanel, BoxLayout.X_AXIS));
@@ -53,22 +59,35 @@ public class AddConsumableUI extends JDialog implements ActionListener {
         JTextField priceField = new JTextField();
         pricePanel.add(priceLabel);
         pricePanel.add(priceField);
-        pricePanel.setPreferredSize(new Dimension(500,100));
+        pricePanel.setPreferredSize(new Dimension(500,30));
 
         JPanel weightOrVolumePanel = new JPanel();
         weightOrVolumePanel.setLayout(new BoxLayout(weightOrVolumePanel, BoxLayout.X_AXIS));
         weightOrVolumeLabel = new JLabel();
-        weightOrVolumeLabel.setText("Weight: ");
+        weightOrVolumeLabel.setText("       ");
         JTextField weightOrVolumeField = new JTextField();
         weightOrVolumePanel.add(weightOrVolumeLabel);
         weightOrVolumePanel.add(weightOrVolumeField);
-        weightOrVolumePanel.setPreferredSize(new Dimension(500,100));
+        weightOrVolumePanel.setPreferredSize(new Dimension(500,30));
+
+        JPanel datePanel = new JPanel();
+        datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.X_AXIS));
+        JLabel dateLabel = new JLabel();
+        dateLabel.setText("Date: ");
+        DatePicker datePicker = new DatePicker();
+        datePicker.addDateChangeListener(this::dateChanged);
+        datePanel.add(dateLabel);
+        datePanel.add(datePicker);
+        datePanel.setPreferredSize(new Dimension (500, 100));
+
+        expDate = datePicker.getDate().atTime(23, 59);
 
         panel.add(consumableTypeSelect);
         panel.add(namePanel);
         panel.add(notesPanel);
         panel.add(pricePanel);
         panel.add(weightOrVolumePanel);
+        panel.add(datePanel);
 
         getContentPane().setSize(500,500);
         getContentPane().add(panel);
@@ -78,14 +97,21 @@ public class AddConsumableUI extends JDialog implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (consumableTypeSelect.getSelectedItem().equals("Food")) {
-            weightOrVolumeLabel.setText("FOOD");
+            weightOrVolumeLabel.setText("Weight: ");
+            isFood = true;
         } else if (consumableTypeSelect.getSelectedItem().equals("Drink")) {
-            weightOrVolumeLabel.setText("DRINK");
+            weightOrVolumeLabel.setText("Volume:");
+            isFood = false;
         }
     }
 
     public Consumable run() {
         this.setVisible(true);
         return ConsumableFactory.getInstance(true, " ", "", 1,1, LocalDateTime.now());
+    }
+
+    @Override
+    public void dateChanged(DateChangeEvent event) {
+
     }
 }
