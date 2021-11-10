51eac236e540da54fdf516b148b17506feac6ee2
kevinchen2002 <kaifengchen2002@gmail.com>
2021-11-10 14:02:37 -0800

    Add invokeLater for threading

--- src/ca/cmpt213/a3/ConsumableTracker.java
+++ src/ca/cmpt213/a3/ConsumableTracker.java
@@ -2,9 +2,16 @@ package ca.cmpt213.a3;
 
 import ca.cmpt213.a3.view.SwingUI;
 
+import javax.swing.*;
+
 public class ConsumableTracker {
     public static void main(String[] args) {
-        SwingUI app = new SwingUI();
-        app.displayMenu();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                SwingUI app = new SwingUI();
+                app.displayMenu();
+            }
+        });
     }
 }
