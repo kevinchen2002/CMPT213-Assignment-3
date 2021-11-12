5a8fe5c7aaa11ec15c9ac43996434f54f5e97ebf
kevinchen2002 <kaifengchen2002@gmail.com>
2021-11-12 11:16:52 -0800

    Add JavaDoc to main class.

--- src/ca/cmpt213/a3/ConsumableTracker.java
+++ src/ca/cmpt213/a3/ConsumableTracker.java
@@ -4,7 +4,16 @@ import ca.cmpt213.a3.view.SwingUI;
 
 import javax.swing.*;
 
+/**
+ * The ConsumableTracker class is the entry point of the system.
+ * Its only responsibility is to call main to initiate the system.
+ */
 public class ConsumableTracker {
+    /**
+     * Creates and calls the SwingUI and terminates system when complete.
+     * This is performed inside invokeLater to utilize the event-dispatching thread.
+     * @param args default argument given by Java
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             SwingUI app = new SwingUI();
